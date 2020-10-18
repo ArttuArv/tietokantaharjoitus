761322d7b8ff748d7557ce3e4b0f049ac40e311a
ArttuArv <t9arar00@students.oamk.fi>
2020-10-18 19:20:30 +0300

    uutta tulee

--- LisaaAsiakas.php
+++ LisaaAsiakas.php
@@ -9,19 +9,19 @@ $con = mysqli_connect($host, $username, $password, $dbname);
 	if (mysqli_connect_errno()) 
 	{
 		echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	}
+	}	
 	
 	$etunimi = mysqli_real_escape_string($con, filter_input(INPUT_POST,'etunimi',FILTER_SANITIZE_STRING));
 	$sukunimi = mysqli_real_escape_string($con, filter_input(INPUT_POST,'sukunimi',FILTER_SANITIZE_STRING));
 	$osoite = mysqli_real_escape_string($con, filter_input(INPUT_POST,'osoite',FILTER_SANITIZE_STRING));
 	$puhelinnumero = mysqli_real_escape_string($con, filter_input(INPUT_POST,'puhnum',FILTER_SANITIZE_STRING));
 	
-	if (empty($etunimi) || empty($sukunimi) || empty($osoite) || empty($puhnum)) 
+	if (empty($etunimi) || empty($sukunimi) || empty($osoite) || empty($puhelinnumero)) 
 	{
 		echo 'Korjaa tyhjät tietueet';
 		return false;
 	}
-	
+
 	$sql = "INSERT INTO Asiakas (Etunimi, Sukunimi, Osoite, Puhelinnumero)
 	VALUES ('$etunimi', '$sukunimi', '$osoite', '$puhelinnumero')";
 	
